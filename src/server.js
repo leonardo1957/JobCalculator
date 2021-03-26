f0b8ceb1fc4258198eb8fc624de7a45028fa4ce9
Leonardo <l30v131r4@gmail.com>
2021-03-26 01:05:15 -0300

    terceiro commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require("express"); // pegando o express
 const server = express(); // rodando a funcao de cima
 
-
+// habilitar arquivos estaticos (criando rotas)
+server.use(express.static("public")) // pegando a pasta public com todo o css e as imagens pra mostrar tudo na index
 
 // request e response
-
 server.get('/', (request, response) => {
-    console.log('entrei')
+    return response.sendFile(__dirname + "/pages/index.html") // dirname = a propria pasta. esse cara ta pegando a pagina src para mostrar quando rodar o server
 
-    return response.send('oi')
 })
+
 server.listen(3000, () => console.log('ta rodando essa bagaca'))
